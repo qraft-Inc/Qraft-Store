0d8c072d3ee4b79fb3660173db937a97615e1df3
tugumeandree <andrewtugume2@gmail.com>
2022-01-31 16:22:52 +0300

    update

--- components/Hero.js
+++ components/Hero.js
@@ -19,7 +19,7 @@ export default function Hero() {
 
         <div className="px-4 py-44 text-left text-white md:max-w-none md:text-center z-10 relative">
           <h1 className="text-4xl font-extrabold leading-10 tracking-tight md:text-center sm:leading-none md:text-6xl lg:text-7xl">
-            <span className="inline-block md:block">Find portfolios of top visual creatives and product designers</span>
+            <span className="inline-block md:block">Find Portfolios Of Top Visual Creatives And Designers</span>
           </h1>
 
           <div className="mx-auto mt-5 md:mt-12 md:max-w-xl md:text-center lg:text-lg mb-12">
