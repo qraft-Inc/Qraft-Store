23ab87b9710c450f0279f73019b7cf5dc98ebfd0
tugumeandree <andrewtugume2@gmail.com>
2022-05-17 18:40:41 +0300

    pivote

--- components/Hero.js
+++ components/Hero.js
@@ -19,11 +19,11 @@ export default function Hero() {
 
         <div className="px-4 py-44 text-left text-white md:max-w-none md:text-center z-10 relative">
           <h1 className="text-4xl font-extrabold leading-10 tracking-tight md:text-center sm:leading-none md:text-6xl lg:text-7xl">
-            <span className="inline-block md:block">Find Portfolios Of Top Visual Creatives And Designers</span>
+            <span className="inline-block md:block">Platform that leads conscious musicians and creative artists to their local and global impact.</span>
           </h1>
 
           <div className="mx-auto mt-5 md:mt-12 md:max-w-xl md:text-center lg:text-lg mb-12">
-          Qraft Store is your online destination to find and showcase creative work and marketplace to the world's best design and visual art creatives
+          Come celebrate the uniqueness, beauty, excellence, colors and diversities of conscious music and art in a safe and inspired atmosphere.
           </div>
 
         </div>
